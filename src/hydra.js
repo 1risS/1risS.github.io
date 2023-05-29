1f6e6aaaeb35be9657bb43a5d31e5cebf6caca09
Iris Saladino <iris.r.saladino@gmail.com>
2023-05-28 21:42:06 -0300

    fix hydra error

--- src/hydra.js
+++ src/hydra.js
@@ -1,14 +1,14 @@
-let canvas = document.getElementById("myCanvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// let canvas = document.getElementById("myCanvas");
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 
-var hydra = new Hydra({
-    canvas: document.getElementById("myCanvas"),
-    detectAudio: false
-  })
+// var hydra = new Hydra({
+//     canvas: document.getElementById("myCanvas"),
+//     detectAudio: false
+//   })
    
-  s0.init({src: document.getElementById("webgl")})
+//   s0.init({src: document.getElementById("webgl")})
 
-  src(s0).modulate(osc().thresh()).out()
+//   src(s0).modulate(osc().thresh()).out()
   
 
