731d0c4ea8528ace7ad2058897aeb170b1f6d77a
Iris Saladino <iris.r.saladino@gmail.com>
2025-07-24 14:54:14 -0300

    fix gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+const isCodeSandbox =
+  'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
 export default {
-    root: 'src/',
-    publicDir: '../public/',
-    base: './',
-    server:
-    {
-        host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
-    },
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    }
+  root: 'src/',
+  publicDir: '../public/',
+  base: 'https://1riss.github.io/portfolio2023/',
+  server: {
+    host: true,
+    open: !isCodeSandbox // Open if it's not a CodeSandbox
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true
+  }
 }
